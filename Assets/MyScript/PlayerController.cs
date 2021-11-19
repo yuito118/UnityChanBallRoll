5d508634ff517bb9e3dad80a25eeda0f8f60f2c4
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-11-19 23:55:04 +0900

    first challenge

---
+++ Assets/MyScript/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH,0,moveV);
+        rb.AddForce(move * speed);
+    }
+}
