b9ece295e49146fb38ec66af106ec4913035b87c
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-11-21 23:45:10 +0900

    unitychan

--- Assets/MyScript/PlayerController.cs
+++ Assets/MyScript/PlayerController.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    int count;
+    public Text countText;
+    AudioSource getSE;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        getSE = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,5 +29,23 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = new Vector3(moveH,0,moveV);
         rb.AddForce(move * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            count ++;
+            SetCountText();
+            getSE.Play();
+        } else if (other.gameObject.CompareTag("Bottom")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "グッド数:" + count.ToString();
     }
 }
